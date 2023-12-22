504393f18c621281d0c9d395abb93c9437914cb3
frevirard <frevirard@gmail.Com>
2023-12-22 20:53:40 +0100

    frederic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './composants/accueil/accueil.component';
+import { ActionsComponent } from './composants/actions/actions.component';
+import { AffichageActionsComponent } from './composants/affichage-actions/affichage-actions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",  component:AccueilComponent},
+  {path:"affichageActions",component:AffichageActionsComponent, loadChildren:()=> import('./composants/affichage-actions/actions.module').then(m=>m.ActionsModule)},
+  // {path:"affichageActions",component:AffichageActionsComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActionsComponent } from './composants/actions/actions.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import { NavComponent } from './composants/nav/nav.component';
+import { MatSelectModule } from '@angular/material/select';
+import { AccueilComponent } from './composants/accueil/accueil.component';
+import {MatCardModule} from '@angular/material/card';
+import { AffichageActionsComponent } from './composants/affichage-actions/affichage-actions.component';
+import { ListeActionsComponent } from './composants/liste-actions/liste-actions.component';
+import {MatIconModule} from '@angular/material/icon';
+import { QuestionnairesComponent } from './composants/questionnaires/questionnaires.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionsComponent,
+    NavComponent,
+    AccueilComponent,
+    AffichageActionsComponent,
+    ListeActionsComponent,
+    QuestionnairesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
